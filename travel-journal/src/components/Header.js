9f8f029d6e76c685efa33e7db9f26aa0d565d004
jkmilbourne <75396369+jkmilbourne@users.noreply.github.com>
2022-01-19 10:14:35 -0700

    Delete travel-journal directory

--- travel-journal/src/components/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return(
-    <header className="header">
-      <img className="header--logo" src="./images/logo.png" alt="logo" />
-      <p>my travel journal.</p>
-    </header>
-  )
-}
--- travel-journal/src/index.js
+++
@@ -1,7 +0,0 @@
-import reactDom from "react-dom"
-
-import App from "./App";
-
-import "./styles/styles.css"
-
-reactDom.render(<App />, document.getElementById("root"));
